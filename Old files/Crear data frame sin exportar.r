1c4d9597cdd210988acc09fc4233ecdcda552609
Diego <88040502+Diegod01@users.noreply.github.com>
2022-02-28 14:20:36 -0300

    Rename Crear data frame sin exportar.r to Old files/Crear data frame sin exportar.r

--- Old files/Crear data frame sin exportar.r
+++ Old files/Crear data frame sin exportar.r
@@ -1,6 +1,6 @@
 Producto = c("Campera", "Pantalon","Campera", "Pantalon","Campera", "Pantalon","Campera")
-Series = c(17,18,20,18,18,18,19) #Vector númerico
-Ventas = c(2,0,0,1,1,1,0)  #Vector númerico
+Series = c(17,18,20,18,18,18,19) #Vector nÃºmerico
+Ventas = c(2,0,0,1,1,1,0)  #Vector nÃºmerico
 d.f1=data.frame(Producto,Series,Ventas) 
 d.f1bk=d.f1 
 str(d.f1)
@@ -12,15 +12,15 @@ str(d.f2) #Volver la variable producto un vector de palabras
 d.f2[c("E1", "E2"),]
 fix(d.f2) #Modificar dataframe
 
-#Añadir filas y columnas
+#AÃ±adir filas y columnas
 nuevas.filas = data.frame(Producto=c("Campera","Campera"),Series =c(18,18),Ventas=c(1,2))
 d.f1=rbrind(d.f1,nuevas.filas)
 d.f1
 
-#Añadir una nueva variable
+#AÃ±adir una nueva variable
 d.f1$Ventas=d.f1$Series*d.f1$Ventas
 d.f1
 d.f1=cbind(d.f1.Grado=rep("Calidad",7))
 d.f1
 
-fix(d.f2) #O para hacerlo más facil, puedes agregar la nueva variable desde el editor
+fix(d.f2) #O para hacerlo mÃ¡s facil, puedes agregar la nueva variable desde el editor
