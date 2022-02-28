366b2565e33a3e187cb9dc78deeb7cea72273845
Diego <88040502+Diegod01@users.noreply.github.com>
2022-02-28 13:01:11 -0300

    Delete Lectura de datos externos.r

--- Lectura de datos externos.r
+++
@@ -1,6 +0,0 @@
-df_pearson2 = read.table("http://aprender.uib.es/Rdir/pearson.txt", header = TRUE)
-View(df_pearson2)
-plot(df_pearson2)
-regHijosPadres = lm(Ventas~Compras, data = df_pearson)
-regHijosPadres = df_pearson2
-abline(regVentasCompras, col = "red"
